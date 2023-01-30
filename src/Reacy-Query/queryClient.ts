6ab33dacd35ea72f0d13b4fed18f75fa95cc7447
Gitseong96 <wkdrl523@gmail.com>
2023-01-30 17:02:55 +0900

    Qeury Option

--- src/Reacy-Query/queryClient.ts
+++ src/Reacy-Query/queryClient.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+   defaultOptions: {
+      queries: {
+         //  staleTime: Infinity,
+         cacheTime: 5 * 60 * 1000,
+      },
+   },
+});
